bc483583c929f1abe632e1fb9ece95c549acb230
Mike P. Sinn <m@quantimodo.com>
2018-02-25 14:53:50 -0600

    Reconfigured for remote master

--- mysql_connector.sh
+++ mysql_connector.sh
@@ -12,42 +12,42 @@ echo "-----------------"
 echo "* Create replication user"
 
 mysql --host mysqlslave -uroot -p$MYSQL_SLAVE_PASSWORD -AN -e 'STOP SLAVE;';
-mysql --host mysqlslave -uroot -p$MYSQL_MASTER_PASSWORD -AN -e 'RESET SLAVE ALL;';
+mysql --host mysqlslave -uroot -p$MYSQL_SLAVE_PASSWORD -AN -e 'RESET SLAVE ALL;';
 
-mysql --host mysqlmaster -uroot -p$MYSQL_MASTER_PASSWORD -AN -e "CREATE USER '$MYSQL_REPLICATION_USER'@'%';"
-mysql --host mysqlmaster -uroot -p$MYSQL_MASTER_PASSWORD -AN -e "GRANT REPLICATION SLAVE ON *.* TO '$MYSQL_REPLICATION_USER'@'%' IDENTIFIED BY '$MYSQL_REPLICATION_PASSWORD';"
-mysql --host mysqlmaster -uroot -p$MYSQL_MASTER_PASSWORD -AN -e 'flush privileges;'
+#mysql --host $MYSQL_MASTER_HOST -u$MYSQL_MASTER_USER -p$MYSQL_MASTER_PASSWORD -AN -e "CREATE USER '$MYSQL_REPLICATION_USER'@'%';"
+#mysql --host $MYSQL_MASTER_HOST -u$MYSQL_MASTER_USER -p$MYSQL_MASTER_PASSWORD -AN -e "GRANT REPLICATION SLAVE ON *.* TO '$MYSQL_REPLICATION_USER'@'%' IDENTIFIED BY '$MYSQL_REPLICATION_PASSWORD';"
+#mysql --host $MYSQL_MASTER_HOST -u$MYSQL_MASTER_USER -p$MYSQL_MASTER_PASSWORD -AN -e 'flush privileges;'
 
 
 echo "* Set MySQL01 as master on MySQL02"
 
-MYSQL01_Position=$(eval "mysql --host mysqlmaster -uroot -p$MYSQL_MASTER_PASSWORD -e 'show master status \G' | grep Position | sed -n -e 's/^.*: //p'")
-MYSQL01_File=$(eval "mysql --host mysqlmaster -uroot -p$MYSQL_MASTER_PASSWORD -e 'show master status \G'     | grep File     | sed -n -e 's/^.*: //p'")
-MASTER_IP=$(eval "getent hosts mysqlmaster|awk '{print \$1}'")
+MYSQL01_Position=$(eval "mysql --host $MYSQL_MASTER_HOST -u$MYSQL_MASTER_USER -p$MYSQL_MASTER_PASSWORD -e 'show master status \G' | grep Position | sed -n -e 's/^.*: //p'")
+MYSQL01_File=$(eval "mysql --host $MYSQL_MASTER_HOST -u$MYSQL_MASTER_USER -p$MYSQL_MASTER_PASSWORD -e 'show master status \G'     | grep File     | sed -n -e 's/^.*: //p'")
+MASTER_IP=$(eval "getent hosts $MYSQL_MASTER_HOST|awk '{print \$1}'")
 echo $MASTER_IP
-mysql --host mysqlslave -uroot -p$MYSQL_SLAVE_PASSWORD -AN -e "CHANGE MASTER TO master_host='mysqlmaster', master_port=3306, \
+mysql --host mysqlslave -uroot -p$MYSQL_SLAVE_PASSWORD -AN -e "CHANGE MASTER TO master_host='$MYSQL_MASTER_HOST', master_port=3306, \
         master_user='$MYSQL_REPLICATION_USER', master_password='$MYSQL_REPLICATION_PASSWORD', master_log_file='$MYSQL01_File', \
         master_log_pos=$MYSQL01_Position;"
 
-echo "* Set MySQL02 as master on MySQL01"
+#echo "* Set MySQL02 as master on MySQL01"
 
-MYSQL02_Position=$(eval "mysql --host mysqlslave -uroot -p$MYSQL_SLAVE_PASSWORD -e 'show master status \G' | grep Position | sed -n -e 's/^.*: //p'")
-MYSQL02_File=$(eval "mysql --host mysqlslave -uroot -p$MYSQL_SLAVE_PASSWORD -e 'show master status \G'     | grep File     | sed -n -e 's/^.*: //p'")
-
-SLAVE_IP=$(eval "getent hosts mysqlslave|awk '{print \$1}'")
-echo $SLAVE_IP
-mysql --host mysqlmaster -uroot -p$MYSQL_MASTER_PASSWORD -AN -e "CHANGE MASTER TO master_host='mysqlslave', master_port=3306, \
-        master_user='$MYSQL_REPLICATION_USER', master_password='$MYSQL_REPLICATION_PASSWORD', master_log_file='$MYSQL02_File', \
-        master_log_pos=$MYSQL02_Position;"
+#MYSQL02_Position=$(eval "mysql --host mysqlslave -uroot -p$MYSQL_SLAVE_PASSWORD -e 'show master status \G' | grep Position | sed -n -e 's/^.*: //p'")
+#MYSQL02_File=$(eval "mysql --host mysqlslave -uroot -p$MYSQL_SLAVE_PASSWORD -e 'show master status \G'     | grep File     | sed -n -e 's/^.*: //p'")
+#
+#SLAVE_IP=$(eval "getent hosts mysqlslave|awk '{print \$1}'")
+#echo $SLAVE_IP
+#mysql --host $MYSQL_MASTER_HOST -u$MYSQL_MASTER_USER -p$MYSQL_MASTER_PASSWORD -AN -e "CHANGE MASTER TO master_host='mysqlslave', master_port=3306, \
+#        master_user='$MYSQL_REPLICATION_USER', master_password='$MYSQL_REPLICATION_PASSWORD', master_log_file='$MYSQL02_File', \
+#        master_log_pos=$MYSQL02_Position;"
 
 echo "* Start Slave on both Servers"
 mysql --host mysqlslave -uroot -p$MYSQL_SLAVE_PASSWORD -AN -e "start slave;"
 
 echo "Increase the max_connections to 2000"
-mysql --host mysqlmaster -uroot -p$MYSQL_MASTER_PASSWORD -AN -e 'set GLOBAL max_connections=2000';
+mysql --host $MYSQL_MASTER_HOST -u$MYSQL_MASTER_USER -p$MYSQL_MASTER_PASSWORD -AN -e 'set GLOBAL max_connections=2000';
 mysql --host mysqlslave -uroot -p$MYSQL_SLAVE_PASSWORD -AN -e 'set GLOBAL max_connections=2000';
 
-mysql --host mysqlslave -uroot -p$MYSQL_MASTER_PASSWORD -e "show slave status \G"
+mysql --host mysqlslave -uroot -p$MYSQL_SLAVE_PASSWORD -e "show slave status \G"
 
 echo "MySQL servers created!"
 echo "--------------------"
